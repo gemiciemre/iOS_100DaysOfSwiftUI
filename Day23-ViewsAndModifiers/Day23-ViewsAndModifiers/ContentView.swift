7830e0146ba47c702e47f462442d6ef39be1fffa
Emre Gemici <emregemici5@gmail.com>
2022-10-25 11:51:09 +0300

    Day23-ViewsAndModifiers Commit

---
+++ Day23-ViewsAndModifiers/Day23-ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Day23-ViewsAndModifiers
+//
+//  Created by Emre Gemici on 17.10.2022.
+//
+
+import SwiftUI
+
+struct Title : ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+extension View {
+    func titleStyle() -> some View{
+        modifier(Title())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .titleStyle()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day23-ViewsAndModifiers/Day23-ViewsAndModifiers/Day23_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day23_ViewsAndModifiersApp.swift
+//  Day23-ViewsAndModifiers
+//
+//  Created by Emre Gemici on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Day23_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
