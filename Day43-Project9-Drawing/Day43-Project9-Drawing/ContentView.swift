ebebfb66e768a022bbb71f7880eb41f49c329358
Emre Gemici <emregemici5@gmail.com>
2023-02-13 15:40:59 +0300

    Project9-Drawing-SpirographCommit

--- Day43-Project9-Drawing/Day43-Project9-Drawing/ContentView.swift
+++ Day43-Project9-Drawing/Day43-Project9-Drawing/ContentView.swift
@@ -7,65 +7,131 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape{
+    var insetAmount : Double
+    var animatableData: Double{
+        get{insetAmount}
+        set{insetAmount = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+            path.move(to: CGPoint(x: 0, y: rect.maxY))
+            path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX-insetAmount, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        return path
+    }
+}
+//-----------------------------
+
+
+struct Checkerboard : Shape{
+    var rows : Int
+    var columns : Int
+    var animatableData: AnimatablePair<Double, Double> {
+    get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+    set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowsize = rect.height / Double(rows)
+        let columnsize = rect.width / Double(columns)
+        
+        for row in 0..<rows{
+            for column in 0..<columns{
+                if(row + column).isMultiple(of: 2){
+                    let startX = columnsize * Double(column)
+                    let startY = rowsize * Double(row)
+                    let rect = CGRect(x: startX, y: startY, width: columnsize, height: rowsize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+//----------------------------
+
+
+struct Spirograph: Shape{
+    
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance : Int
+    let amount : Double
+    
+    func gcd(_ a: Int, _ b: Int)->Int{
+        var a = a
+        var b = b
+        
+        while b != 0{
+            let temp = b
+            b = a%b
+            a = temp
+        }
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius-outerRadius
+        let endPoint = ceil(2*Double.pi*outerRadius / Double(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01){
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            }
+            else{
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        return path
+    }
+}
+    
+//---------------------------
+    
+
+
+
+
 struct ContentView: View {
+    
     @State private var amount = 0.0
     @State private var insetAmount = 50.0
     @State private var rows = 4
     @State private var columns = 4
     
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amountSpirograph = 1.0
+    @State private var hue = 0.6
     
-    struct Trapezoid: Shape{
-        var insetAmount : Double
-        var animatableData: Double{
-            get{insetAmount}
-            set{insetAmount = newValue}
-        }
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            
-                path.move(to: CGPoint(x: 0, y: rect.maxY))
-                path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-                path.addLine(to: CGPoint(x: rect.maxX-insetAmount, y: rect.minY))
-                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-                path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-            return path
-        }
-    }
-    
-    struct Checkerboard : Shape{
-        var rows : Int
-        var columns : Int
-        var animatableData: AnimatablePair<Double, Double> {
-        get {
-               AnimatablePair(Double(rows), Double(columns))
-            }
-
-        set {
-                rows = Int(newValue.first)
-                columns = Int(newValue.second)
-            }
-        }
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            
-            let rowsize = rect.height / Double(rows)
-            let columnsize = rect.width / Double(columns)
-            
-            for row in 0..<rows{
-                for column in 0..<columns{
-                    if(row + column).isMultiple(of: 2){
-                        let startX = columnsize * Double(column)
-                        let startY = rowsize * Double(row)
-                        let rect = CGRect(x: startX, y: startY, width: columnsize, height: rowsize)
-                        path.addRect(rect)
-                    }
-                }
-            }
-            return path
-        }
-    }
+ 
     
     var body: some View {
 //  Speacial Effects:blurs, blending
@@ -104,13 +170,46 @@ struct ContentView: View {
 //                }
 //            }
         
-        Checkerboard(rows: rows, columns: columns)
-            .onTapGesture {
-                withAnimation(.linear(duration: 3)){
-                    rows = 8
-                    columns = 16
-                }
+//        Checkerboard(rows: rows, columns: columns)
+//            .onTapGesture {
+//                withAnimation(.linear(duration: 3)){
+//                    rows = 8
+//                    columns = 16
+//                }
+//            }
+        
+        VStack{
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amountSpirograph)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1),lineWidth: 1)
+                .frame(width: 300,height: 300)
+            
+            Spacer()
+            
+            Group{
+                Text("Inner Radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal,.bottom])
+                
+                Text("Outer Radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal,.bottom])
+                
+                Text("Distance: \(Int(distance))")
+                Slider(value:$distance, in: 1...150, step: 1)
+                    .padding([.horizontal,.bottom])
+                
+                Text("Amount: \(amountSpirograph, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amountSpirograph)
+                    .padding([.horizontal,.bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
             }
+        }
+        
         
     }
 }
