c51f7f2f4dac49a6c3540c70280b06467ca9ff90
Emre Gemici <emregemici5@gmail.com>
2023-05-18 14:24:50 +0300

    Bucketlist - Day70 - Commit

---
+++ Day68-Project14-Bucketlist/Day68-Project14-Bucketlist/Day68_Project14_BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day68_Project14_BucketlistApp.swift
+//  Day68-Project14-Bucketlist
+//
+//  Created by Emre Gemici on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Day68_Project14_BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
